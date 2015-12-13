ba278d4a11322458c87c0390f6468ed780fa4e85
thomasvaeth <thomas.vaeth@gmail.com>
2015-12-12 17:22:02 -0800

    Added Mailchimp.

---
+++ js/coming-soon.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#mc-form').ajaxChimp({
+		url: 'http://gradingclub.us10.list-manage.com/subscribe/post?u=a27abf4ea679c7b902db61f62&amp;id=6d23e28cdf'
+	});
+});
---
+++ js/jquery.ajaxchimp.js
@@ -0,0 +1,124 @@
+/*!
+Mailchimp Ajax Submit
+jQuery Plugin
+Author: Siddharth Doshi
+Use:
+===
+$('#form_id').ajaxchimp(options);
+- Form should have one <input> element with attribute 'type=email'
+- Form should have one label element with attribute 'for=email_input_id' (used to display error/success message)
+- All options are optional.
+Options:
+=======
+options = {
+    language: 'en',
+    callback: callbackFunction,
+    url: 'http://blahblah.us1.list-manage.com/subscribe/post?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f'
+}
+Notes:
+=====
+To get the mailchimp JSONP url (undocumented), change 'post?' to 'post-json?' and add '&c=?' to the end.
+For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f&c=?',
+*/
+
+(function ($) {
+	'use strict';
+
+	$.ajaxChimp = {
+		responses: {
+			'We have sent you a confirmation email.' : 0,
+			'Please enter a valid email address' : 1,
+			'Please enter a valid email address' : 2,
+			'Please enter a valid email address' : 3,
+			'Please enter a valid email address' : 4,
+			'Please enter a valid email address' : 5
+		},
+		translations: {
+			'en': null
+		},
+		init: function (selector, options) {
+			$(selector).ajaxChimp(options);
+		}
+	};
+
+	$.fn.ajaxChimp = function (options) {
+		$(this).each(function(i, elem) {
+			var form = $(elem);
+			var email = form.find('input[type=email]');
+			var label = form.find('label[for=' + email.attr('id') + ']');
+
+			var settings = $.extend({
+				'url': form.attr('action'),
+				'language': 'en'
+			}, options);
+
+			var url = settings.url.replace('/post?', '/post-json?').concat('&c=?');
+
+			form.attr('novalidate', 'true');
+			email.attr('name', 'EMAIL');
+
+			form.submit(function () {
+				var msg;
+				function successCallback(resp) {
+					if (resp.result === 'success') {
+						msg = 'We have sent you a confirmation email';
+						label.removeClass('error').addClass('valid');
+						email.removeClass('error').addClass('valid');
+					} else {
+						email.removeClass('valid').addClass('error');
+						label.removeClass('valid').addClass('error');
+						var index = -1;
+						try {
+							var parts = resp.msg.split(' - ', 2);
+							if (parts[1] === undefined) {
+								msg = 'Please enter a valid email address';
+							} else {
+								var i = parseInt(parts[0], 10);
+								if (i.toString() === parts[0]) {
+									index = parts[0];
+									msg = 'Please enter a valid email address';
+								} else {
+									index = -1;
+									msg = 'Please enter a valid email address';
+								}
+							}
+						}
+						catch (e) {
+							index = -1;
+							msg = 'Please enter a valid email address';
+						}
+					}
+
+					// Translate and display message
+					if (settings.language !== 'en' && $.ajaxChimp.responses[msg] !== undefined && $.ajaxChimp.translations && $.ajaxChimp.translations[settings.language] && $.ajaxChimp.translations[settings.language][$.ajaxChimp.responses[msg]]) {
+						msg = $.ajaxChimp.translations[settings.language][$.ajaxChimp.responses[msg]];
+					}
+					label.html(msg);
+					label.show(2000);
+					if (settings.callback) {
+						settings.callback(resp);
+					}
+				}
+
+				var data = {};
+				var dataArray = form.serializeArray();
+				$.each(dataArray, function (index, item) {
+					data[item.name] = item.value;
+				});
+
+				$.ajax({
+					url: url,
+					data: data,
+					success: successCallback,
+					dataType: 'jsonp',
+					error: function (resp, text) {
+						console.log('mailchimp ajax submit error: ' + text);
+					}
+				});
+
+				return false;
+			});
+		});
+		return this;
+	};
+})(jQuery);
