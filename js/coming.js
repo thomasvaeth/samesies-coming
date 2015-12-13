f2bb289f944b62d7809d19d0389bffb4e48c5586
thomasvaeth <thomas.vaeth@gmail.com>
2015-12-12 17:30:23 -0800

    Added Final Countdown.

--- js/coming.js
+++ js/coming.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('#mc-form').ajaxChimp({
 		url: 'http://gradingclub.us10.list-manage.com/subscribe/post?u=a27abf4ea679c7b902db61f62&amp;id=6d23e28cdf'
 	});
+
+	$('#getting-started').countdown('2016/01/01', function(e) {
+		$(this).html(e.strftime('%w weeks %d days %H:%M:%S'));
+	});
 });
