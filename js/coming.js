3359b02a83ef9c81ce9c1f019a69e0c1de0b4094
thomasvaeth <thomas.vaeth@gmail.com>
2015-12-13 11:48:30 -0800

    Updated ajaxChimp.

--- js/coming.js
+++ js/coming.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
 	});
 
 	$('#mc-form').ajaxChimp({
-		url: 'http://gradingclub.us10.list-manage.com/subscribe/post?u=a27abf4ea679c7b902db61f62&amp;id=6d23e28cdf'
+		url: 'http://YOURMAILCHIMPHERE.us10.list-manage.com/subscribe/post?u=YOURMAILCHIMPHERE&amp;id=YOURMAILCHIMPHERE'
 	});
 });
